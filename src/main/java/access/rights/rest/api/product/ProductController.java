e27361d8c5b835355029cfaa0ce7def579eebc21
Marko <mkjovanov@gmail.com>
2018-08-10 16:43:06 +0200

    Added response from Product API
    - Added ResponseEntity from all the rest actions on ProductController and ProductService

--- src/main/java/access/rights/rest/api/product/ProductController.java
+++ src/main/java/access/rights/rest/api/product/ProductController.java
@@ -3,6 +3,7 @@ package access.rights.rest.api.product;
 import access.rights.rest.api.organization.entities.Organization;
 import access.rights.rest.api.product.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,41 +15,45 @@ public class ProductController {
     private ProductService productService;
 
     @RequestMapping("/organizations/{organizationId}/products")
-    public List<Product> getAllProducts(@PathVariable("organizationId") String organizationId) {
+    @ResponseBody
+    public ResponseEntity getAllProducts(@PathVariable("organizationId") String organizationId) {
         return productService.getAllProducts(organizationId);
     }
 
     @RequestMapping("/organizations/{organizationId}/products/{id}")
-    public Product getProduct(@PathVariable("organizationId") String organizationId, @PathVariable("id") String id) {
+    @ResponseBody
+    public ResponseEntity getProduct(@PathVariable("organizationId") String organizationId,
+                                     @PathVariable("id") String id) {
         return productService.getProduct(organizationId, id);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/organizations/{organizationId}/products")
-    public void addNewProduct(@PathVariable("organizationId") String organizationId, @RequestBody Product newProduct) {
+    public ResponseEntity addNewProduct(@PathVariable("organizationId") String organizationId,
+                                    @RequestBody Product newProduct) {
         newProduct.setOrganization(new Organization(organizationId, ""));
-        //TODO: Get logged in user access rights object
-        productService.addProduct(organizationId, newProduct);
-    }
-
-    @RequestMapping(method = RequestMethod.PATCH, value = "/organizations/{organizationId}/products")
-    public void addNewProductList(@PathVariable("organizationId") String organizationId, @RequestBody List<Product> productList) {
-        productList.forEach(x -> {
-            x.setOrganization(new Organization(organizationId, ""));
-        });
-        for (Product product : productList) {
-            productService.addProduct(organizationId, product);
-        }
+        return productService.addProduct(organizationId, newProduct);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/organizations/{organizationId}/products/{id}")
-    public void updateProduct(@PathVariable("organizationId") String organizationId, @RequestBody Product updatedProduct, @PathVariable String id) {
-        //TODO: is this needed?
+    public ResponseEntity updateProduct(@PathVariable("organizationId") String organizationId,
+                                        @RequestBody Product updatedProduct,
+                                        @PathVariable String id) {
         updatedProduct.setOrganization(new Organization(organizationId, ""));
-        productService.updateProduct(id, organizationId, updatedProduct);
+        return productService.updateProduct(id, organizationId, updatedProduct);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/organizations/{organizationId}/products/{id}")
-    public void deleteProduct(@PathVariable("id") String id, @PathVariable("organizationId") String organizationId) {
-        productService.deleteProduct(id, organizationId);
+    public ResponseEntity deleteProduct(@PathVariable("id") String id,
+                              @PathVariable("organizationId") String organizationId) {
+        return productService.deleteProduct(id, organizationId);
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "/organizations/{organizationId}/products")
+    public void addNewProductList(@PathVariable("organizationId") String organizationId,
+                                  @RequestBody List<Product> productList) {
+        productList.forEach(x -> x.setOrganization(new Organization(organizationId, "")));
+        for (Product product : productList) {
+            productService.addProduct(organizationId, product);
+        }
     }
 }
--- src/main/java/access/rights/rest/api/product/ProductService.java
+++ src/main/java/access/rights/rest/api/product/ProductService.java
@@ -5,6 +5,8 @@ import access.rights.rest.api.access.rights.entities.CrudOperation;
 import access.rights.rest.api.product.entities.Product;
 import access.rights.rest.api.product.repositories.ProductInMemoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,33 +19,43 @@ public class ProductService {
     @Autowired
     private AccessRightsService accessRightsService;
 
-    public List<Product> getAllProducts(String organizationId) {
-        return accessRightsService.filterByAccessRights(organizationId);
+    public ResponseEntity<List<Product>> getAllProducts(String organizationId) {
+        List<Product> availableProducts = accessRightsService.filterByAccessRights(organizationId);
+        if (availableProducts.isEmpty()) {
+            return new ResponseEntity("Reading products from organization: '" + organizationId + "' is forbidden for this user", HttpStatus.FORBIDDEN);
+        }
+        return new ResponseEntity(availableProducts, HttpStatus.ACCEPTED);
     }
 
-    public Product getProduct(String organizationId, String id) {
-        if (accessRightsService.isCrudOperationAvailable(CrudOperation.Read, organizationId)) {
-            return productRepository.get(id);
+    public ResponseEntity<Product> getProduct(String organizationId, String id) {
+        if (!accessRightsService.isCrudOperationAvailable(CrudOperation.Read, organizationId)) {
+            return new ResponseEntity("Reading product from organization: '" + organizationId + "' is forbidden for this user", HttpStatus.FORBIDDEN);
         }
-        return null;
+        return new ResponseEntity(productRepository.get(id), HttpStatus.ACCEPTED);
     }
 
-    public void addProduct(String organizationId, Product newProduct) {
-        if (accessRightsService.isCrudOperationAvailable(CrudOperation.Create, organizationId)) {
-            productRepository.add(newProduct);
+    public ResponseEntity addProduct(String organizationId, Product newProduct) {
+        if (!accessRightsService.isCrudOperationAvailable(CrudOperation.Create, organizationId)) {
+            return new ResponseEntity("Adding new product from organization: '" + organizationId + "' is forbidden for this user", HttpStatus.FORBIDDEN);
         }
+        productRepository.add(newProduct);
+        return new ResponseEntity(newProduct, HttpStatus.CREATED);
     }
 
-    public void updateProduct(String id, String organizationId, Product updatedProduct) {
-        if (accessRightsService.isCrudOperationAvailable(CrudOperation.Update, organizationId)) {
-            productRepository.update(id, updatedProduct);
+    public ResponseEntity updateProduct(String id, String organizationId, Product updatedProduct) {
+        if (!accessRightsService.isCrudOperationAvailable(CrudOperation.Update, organizationId)) {
+            return new ResponseEntity("Updating product from organization: '" + organizationId + "' is forbidden for this user", HttpStatus.FORBIDDEN);
         }
+        productRepository.update(id, updatedProduct);
+        return new ResponseEntity(updatedProduct, HttpStatus.ACCEPTED);
     }
 
-    public void deleteProduct(String id, String organizationId) {
-        if (accessRightsService.isCrudOperationAvailable(CrudOperation.Delete, organizationId)) {
-            productRepository.delete(id);
+    public ResponseEntity deleteProduct(String id, String organizationId) {
+        if (!accessRightsService.isCrudOperationAvailable(CrudOperation.Delete, organizationId)) {
+            return new ResponseEntity("Deleting product from organization: '" + organizationId + "' is forbidden for this user", HttpStatus.FORBIDDEN);
         }
+        productRepository.delete(id);
+        return new ResponseEntity(id, HttpStatus.ACCEPTED);
     }
 
     public List<Product> getAllProductsBypassAccessRights(String organizationId) {
